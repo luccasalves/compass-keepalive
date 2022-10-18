ca440f955eb542d6296fd45791aa852df9d0ca5a
Lucas Alves <as.lucasalves@gmail.com>
2022-10-18 17:27:34 -0300

    adicionado o texto de boas vindas no login

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,12 +1,16 @@
 import { create } from "../utils/dom";
 import "../styles/authPage.scss";
 import { LoginForm } from "../components/login-form";
+import { TextApp } from "../components/text";
 
 export function AuthPage() {
   const authPage = create("main", "auth-page");
   const asideBg = create("div", "aside-bg");
   const bgAuth = document.createElement("img");
   const asideLogin = document.createElement("aside");
+  const welcomeBox = document.createElement("div");
+
+  welcomeBox.classList.add("welcome-box");
   asideLogin.classList.add("aside-login");
   bgAuth.classList.add("bg");
   bgAuth.setAttribute("src", "src/assets/bg.png");
@@ -15,7 +19,16 @@ export function AuthPage() {
   logo.classList.add("logo");
   logo.setAttribute("src", "./src/assets/logo-compasso.svg");
 
-  asideLogin.append(LoginForm());
+  welcomeBox.append(
+    TextApp("p", "welcome", "Olá"),
+    TextApp(
+      "p",
+      "label-welcome",
+      "Para continuar navegando de forma segura, efetue o login na rede."
+    )
+  );
+
+  asideLogin.append(welcomeBox, LoginForm());
   asideBg.append(bgAuth, logo);
   authPage.append(asideLogin, asideBg);
 
